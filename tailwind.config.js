4d59cd2782034ab0432bf9a07493857294ef0026
Simran145678 <harsimrankaur708@gmail.com>
2024-05-18 16:48:09 -0400

    added cards in about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
       fontFamily:{
         noto :['Noto Serif', 'serif']
       },
+      boxShadow: {
+        100: 'rgba(255, 251, 233, 0.1) 0px 10px 15px -3px inset, rgba(255, 251, 233, 0.1) 0px 4px 6px -4px;'
+      },
       opacity:{
         0 :'0',
         100:'100'
